c2a1aac860d8de09ae7ea5a67025cbf9325069f6
Romain Cazier <romain@eprc.studio>
2023-01-15 16:25:12 +0100

    Small css update when two folders are next to each other

--- TagsToFolders.info.php
+++ TagsToFolders.info.php
@@ -3,7 +3,7 @@
 $info = array(
 	"title" => "Tags to Folders",
 	"author" => "EPRC",
-	"version" => "0.0.2",
+	"version" => "0.0.3",
 	"summary" => "This is a small helper tool to visually organise fields/templates into folders in the menu when they are tagged",
 	"href" => "https://github.com/romaincazier/TagsToFolders",
 	"icon" => "folder",
