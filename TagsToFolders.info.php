c165b2d21cc571196e2c5cf7b5a9054863fd0ada
Romain Cazier <romain@eprc.studio>
2024-07-25 12:07:43 +0200

    No need to autoload everywhere, only in the admin

--- TagsToFolders.info.php
+++ TagsToFolders.info.php
@@ -3,10 +3,10 @@
 $info = array(
 	"title" => "Tags to Folders",
 	"author" => "EPRC",
-	"version" => "0.0.6",
+	"version" => "0.0.7",
 	"summary" => "This is a small helper tool to visually organise fields/templates into folders in the menu when they are tagged",
 	"href" => "https://github.com/eprcstudio/TagsToFolders",
 	"icon" => "folder",
-	"autoload" => true,
+	"autoload" => "template=admin",
 	"singular" => true
 );
